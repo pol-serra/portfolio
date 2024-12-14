1454bdd321a9d034a3e863b648cf9d5c4c4c8fb5
Pol Serra Montes <polserramontes@MacBook-MacBook-Pro-de-Pol-3.local>
2024-12-15 00:30:08 +0100

    Second stage changes

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,18 +13,18 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:myemail@email.com">myemail@email.com</a>
+          <a href="mailto:polsm9@gmail.com">polsm9@gmail.com</a>
         </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/myname">linkedin.com/myname</a>
+          <a href="https://www.linkedin.com/in/pol-serra-montes/">linkedin.com/pol-serra-montes</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/myname">github.com/myname</a>
+          <a href="https://www.github.com/https://github.com/pol-serra">github.com/pol-serra</a>
         </li>
       </ul>
     </footer>
